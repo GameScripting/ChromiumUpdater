1ff7cb8993d701d41d2e8e75341714ef308434fc
Daniel Huhn <daniel_huhn@outlook.com>
2013-12-08 18:14:53 +0100

    This scripts installs the latest version of chromium if its not already installed

---
+++ updateChromium.js
@@ -0,0 +1,72 @@
+var request = require('request');
+var fs = require('fs');
+var child_process = require('child_process');
+
+var getLatestRevision = function(callback){
+    
+    request('http://chromium.woolyss.com/download/', function(err, res, body) {
+        if(err) throw err;
+
+        var findLatestRevisionRegEx = new RegExp('chromium-browser-continuous/Win_x64/(\\d{1,})');
+        var match = body.match(findLatestRevisionRegEx);
+        if(match){
+            var revision = match[1];
+            callback(revision);
+        }
+    });
+};
+
+var installCheckRevision = function(revision, callback){
+    var installed = __dirname + '/INSTALLED'
+    
+    if(fs.exists(installed, function(exists){
+        
+        var saveCurrentRev = function(cb) {
+            fs.writeFile(installed, revision, cb);
+        };
+        
+        if(!exists){
+            saveCurrentRev(function(){
+               callback(true); 
+            });
+        } else {
+            fs.readFile(installed, function(err, data){
+                if(err) throw err;
+                
+                var needsUpdate = data < revision;
+                
+                if(needsUpdate) {
+                    saveCurrentRev(function(){
+                        callback(true);
+                    })
+                } else {
+                    callback(false);
+                }
+            });
+        }
+    }));
+};
+
+getLatestRevision(function(revision){
+    var installerUrl = 'https://commondatastorage.googleapis.com/chromium-browser-continuous/Win_x64/'+revision+'/mini_installer.exe';
+    var filename = __dirname + '/' + revision + '_installer.exe';
+    
+    installCheckRevision(revision, function(needsUpdate){
+        
+        if(!needsUpdate){
+            console.log("Chromium is already up-to-date :)");
+            return;
+        }
+        
+        console.log('Downloading installer ...');
+        request(installerUrl).pipe(fs.createWriteStream(filename)).on('finish', function() {
+            console.log('Installer saved!');
+                
+            var installProcess = child_process.spawn(filename);
+            installProcess.on('close', function(){
+                console.log('Chromium ist now up-to-date!');
+            });
+        });
+        
+    })
+});
